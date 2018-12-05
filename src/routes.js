ca601407ce975ce5842e8a4292c367c5c910187d
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-05 18:13:25 -0200

    Created the new page Search

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {Route, Switch} from 'react-router-dom';
 
 //Components
 import Home from './pages/Home'
+import Search from './pages/Search'
 
 const Routes = () =>  {
 
   return(
     <Switch>
       <Route path='/' exact component={Home}/>
+      <Route path='/search' component={Search}/>
     </Switch>
   );
 }
