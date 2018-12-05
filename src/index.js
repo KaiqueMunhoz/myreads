2827017bb172babbaa8aa584ce69d7c7a62f132d
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-05 17:27:56 -0200

    Using routes in the index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//Routes
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+
+ReactDOM.render(
+  <BrowserRouter>
+      <Routes />
+  </BrowserRouter>
+  , document.getElementById('root'));
