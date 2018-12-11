8ea242c70924f89e6a3a3a13a15550d0e50a418b
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-11 13:46:30 -0200

    Change the component to stateless

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,17 +5,12 @@ import './Home.css'
 //Components
 import ListBooks from '../../components/ListBooks';
 
-class Home extends React.Component {
-  state = {
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <ListBooks />
-      </div>
-    )
-  }
+const Home = () => {
+  return (
+    <div className="app">
+      <ListBooks />
+    </div>
+  )
 }
 
 export default Home
