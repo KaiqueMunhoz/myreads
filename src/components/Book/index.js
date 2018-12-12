785a242a65d8ea77c1825bdd91e9a15af51b59f2
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-12 16:30:08 -0200

    Stateless to component, added a function that get the option selected

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,30 +1,36 @@
 import React from 'react'
 
-const Book = (props) => {
+class Book extends React.Component {
 
-  const {book} = props
+  moveBookshelf = (event) => {
+    console.log(event.target.value)
+  }
   
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          {/* Retirar esse estilo inline */}
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+  render() {
+    const {book} = this.props
+
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            {/* Retirar esse estilo inline */}
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
+            <div className="book-shelf-changer">
+              <select onChange={this.moveBookshelf}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
+          <div className="book-title">{book.title}</div>
+          {book.authors.map(author => <div className="book-authors">{author}</div> )}
         </div>
-        <div className="book-title">{book.title}</div>
-        {book.authors.map(author => <div className="book-authors">{author}</div> )}
-      </div>
-    </li>
-  )
+      </li>
+    )
+  }
 }
 
 export default Book
