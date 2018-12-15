41e3aded9d91e5f6ce357f2a434a2f2c8bd72e44
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-15 19:49:52 -0200

    Moved a function for the parent

--- src/components/Bookshelf/index.js
+++ src/components/Bookshelf/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 
+
 //Components
 import Book from '../Book'
 
 const Bookshelf = (props) => {
 
-  const {title, books} = props
+  const {title, books, moveBookshelf} = props
 
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {books.map(book => <Book key={book.id} book={book}/>)}
+          {books.map(book => 
+            <Book 
+              book={book}
+              key={book.id} 
+              moveBookshelf={moveBookshelf} />
+          )}
         </ol>
       </div>
     </div>
