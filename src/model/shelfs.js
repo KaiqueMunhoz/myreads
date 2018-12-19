77e4f942cb305d3dff2ffa10a31a059a5c7aaf4d
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-19 16:46:03 -0200

    Created a model to shelfs

---
+++ src/model/shelfs.js
@@ -0,0 +1,16 @@
+const shelfs = [
+  {
+    title: "Current reading",
+    apiValue: "currentlyReading"
+  },
+  {
+    title: "Want to read",
+    apiValue: "wantToRead"
+  },
+  {
+    title: "Read",
+    apiValue: "read"
+  }
+]
+
+export default shelfs
