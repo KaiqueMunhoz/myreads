aa050c7282cad32d05898cd6b6a23f8003f8eb9f
Kaique Munhoz <munhoz13km@gmail.com>
2018-12-29 19:07:16 -0200

    Extract the bookshelf to a variable

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,24 +12,23 @@ import shelfs from '../../model/shelfs'
 const Home = (props) => {
 
     const {moveBookshelf} = props
-
     const filterBooksByShelf = (apiValue) => props.books.filter(book =>  book.shelf === apiValue )    
+    const bookshelfs = shelfs.map(shelf => {
+      const books = filterBooksByShelf(shelf.apiValue)
+      return shelf.apiValue !== 'none' &&
+        <Bookshelf 
+          books={books}
+          key={shelf.title + shelf.apiValue}
+          moveBookshelf={moveBookshelf}
+          title={shelf.title} /> 
+    })
     
     return (
       <div className="app">
         <div className="list-books">
         <ListBooksTitle />
         <div className="list-books-content">
-          {shelfs.map(shelf => {
-            const books = filterBooksByShelf(shelf.apiValue)
-            return shelf.apiValue !== 'none' &&
-              <Bookshelf 
-                books={books}
-                key={shelf.title + shelf.apiValue}
-                moveBookshelf={moveBookshelf}
-                title={shelf.title} /> 
-            })
-          })}
+          {bookshelfs}
         </div>
         <ButtonSearch />
       </div>
