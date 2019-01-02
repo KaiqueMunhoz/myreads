aa224bcb0b8b7afb56e83c0adf69c45282a4ba24
Kaique Munhoz <munhoz13km@gmail.com>
2019-01-02 18:00:39 -0200

    Extract the authors to a function

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -8,6 +8,12 @@ const Book = (props) => {
 
     const {book, moveBookshelf} = props
     const image = book.imageLinks
+    const showAuthors = () => {
+      if(!book.authors) {
+        return ''
+      }
+      return book.authors.map((author, id) => <div key={author + book.id} className="book-authors">{id + " " + author}</div> )
+    }
     
     return (
       <li>
@@ -16,10 +22,8 @@ const Book = (props) => {
             <img className="book-cover" src={!image ? '': image.thumbnail} alt='Book Cover' />
             <BookSelect book={book} moveBookshelf={moveBookshelf}/>
           </div>
-           <div className="book-title">{book.title}</div>
-           {/* Arrumar o if aqui depois */}
-          {book.authors ? 
-            book.authors.map(author => <div key={author + book.id} className="book-authors">{author}</div> ): ''}
+          <div className="book-title">{book.title}</div>
+          {showAuthors}
         </div>
       </li>
     )
