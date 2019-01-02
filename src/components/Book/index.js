3236aa4eb025d0c91be2544dcff629bc7d4f8f9d
Kaique Munhoz <munhoz13km@gmail.com>
2019-01-02 19:02:52 -0200

    Fix the react child warning

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -16,16 +16,18 @@ const Book = (props) => {
   }
   
   return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <img className="book-cover" src={!image ? '': image.thumbnail} alt='Book Cover' />
-          <BookSelect book={book} moveBookshelf={moveBookshelf}/>
+    <div>
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <img className="book-cover" src={!image ? '': image.thumbnail} alt='Book Cover' />
+            <BookSelect book={book} moveBookshelf={moveBookshelf}/>
+          </div>
+          <div className="book-title">{book.title}</div>
+          {showAuthors}
         </div>
-        <div className="book-title">{book.title}</div>
-        {showAuthors}
-      </div>
-    </li>
+      </li>
+    </div>
   )
 }
 
