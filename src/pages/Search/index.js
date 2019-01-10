d5fb39fb07503f1cd45ff11f01ebe30978ee52be
Kaique Munhoz <munhoz13km@gmail.com>
2019-01-10 16:59:36 -0200

    Removed `container-fluid`

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -50,7 +50,7 @@ class Search extends React.Component {
                   )
           
     return (
-      <div className="search-books container-fluid">
+      <div className="search-books">
         <SearchBooksBar searchBooks={this.searchBooks}/>
         <div className="search-books-results">
           <ol className="books-grid">
